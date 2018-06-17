6fb6762ce711f09829c4ff87770ca2793dca1173
Sandra Peterson <writesandy@gmail.com>
2018-06-16 21:19:02 -0500

    adding to server.js code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const request = require("request");
 
 
 //Our scraping tools
@@ -9,7 +10,8 @@ const cheerio = require("cheerio");
 //Require all models
 const db = require("./models");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Initialize Express
 const app = express();
@@ -17,4 +19,25 @@ const app = express();
 // Configure middleware
 
 // request for logging requests
+// app.use(request(""));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true}));
+// Use express.static to serve the public folder as a static directoryy
+app.use(express.static("publc"));
+// Connect to Mongo DB
+mongoose.connect("mongodb://localhost/3000")
 
+// Routes
+
+// GET route for scraping website
+app.get("/scrape", function (req, res) {
+    request.get("http://www.echojs.com/").then(function(res){
+        console.log(res)
+    })
+})
+
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
